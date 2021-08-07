e5469e2c68a1ead4e983539d1ec8e7730994ec45
Luddifee <ludwig@ludwig-lehnert.de>
2021-08-07 18:33:09 +0200

    added .gitignore

--- app/src/main/java/de/ludklas/forty_two_launcher/MainActivity.kt
+++ app/src/main/java/de/ludklas/forty_two_launcher/MainActivity.kt
@@ -1,7 +1,5 @@
 package de.ludklas.forty_two_launcher
 
-import android.graphics.Color
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
